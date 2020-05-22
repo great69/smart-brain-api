43314e76e7782c9a467d886a155a25684ed3a0af
Great Thavapatikom <wondergreat69@gmail.com>
2020-05-22 11:31:52 +0700

    change knex route to heroku postgres

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
                   client: 'pg',
                   connection: {
-                    host : '127.0.0.1', //localhost
+                    host : 'postgresql-animate-92521', //127.0.0.1 if want to use localhost
                     user : 'postgres',
                     password : 'test',
                     database : 'smartbrain'
